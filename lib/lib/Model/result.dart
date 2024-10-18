1f6c06fe847493a692842ebf53734c3b054994f9
graduationteam25 <graduationteam229@gmail.com>
2024-10-18 10:53:24 +0200

    Delete lib/lib directory

--- lib/lib/Model/result.dart
+++
@@ -1,7 +0,0 @@
-class ResultPage {
-  ResultPage(
-      {required this.photo, required this.dateTime, required this.state});
-  final String photo;
-  final String dateTime;
-  final String state;
-}
